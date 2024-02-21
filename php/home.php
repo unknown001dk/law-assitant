8f3b5642da3b59c118374f93959f7c21fbe51ca1
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-02-21 21:54:12 +0530

    frontend work completed

---
+++ php/home.php
@@ -0,0 +1,17 @@
+<?php
+    $set_title = "Home-Page";
+
+    include("../common/head.php");
+    require("../html/header.html");
+    //include("../connections/login.php");
+?>
+<?php
+require("../pages/article.html");
+require("../html/index.html");
+?>
+
+
+<?php
+
+require("../html/footer.html");
+?>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../chat-online/**/*.{html,js,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
